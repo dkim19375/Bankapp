9f88d4ff22181ea52fbdde65ae643fe61440c807
hydrate <arthmorgt2@gmail.com>
2021-09-02 19:30:59 +0200

    added unit tests

---
+++ src/test/java/me/hydra/Account/DoesThisWork.java
@@ -0,0 +1,17 @@
+//package me.hydra.Account;
+
+import hydrate.Account;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//public class DoesThisWork {
+
+  //  @Test
+   // void calculateInterest() {
+    //    Account account = new Account();
+  //      account.calculateInterest(50);
+   //     assertEquals(50, account.calculateInterest(50));
+        
+   // }
+//}
