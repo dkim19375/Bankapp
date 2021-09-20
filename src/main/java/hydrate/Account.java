efadef41db2546da6438332833e5d198ddbc1f6e
dkim19375 <miner8mk@gmail.com>
2021-09-20 16:31:44 -0400

    add finals, encapsulation (although you're not using the variables) and fix the code that odin modified

--- src/main/java/hydrate/Account.java
+++ src/main/java/hydrate/Account.java
@@ -1,54 +1,50 @@
 package hydrate;
 
-
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Account {
 
     //todo use these fields or remove them!!
+    private int depositTransaction;
+    private int withdrawTransaction;
+    private int balance;
+    private int previousTransaction;
+    private String customerName;
+    private String customerID;
+    private Scanner amount; // why is this a Scanner?
 
-    public int depositTransaction;
-    public int withdrawTransaction;
-    int balance;
-    int previousTransaction;
-    String customerName;
-    String customerID;
-    Scanner amount;
-    String option;
-
-    void deposit(Scanner sc) {
+    public void deposit(final Scanner sc) {
+        // TODO Recommendation: Keep the IO stuff (Scanner) out of this by replacing the Scanner parameter with int
         System.out.println("deposit an amount of money: ");
-        int amount = sc.nextInt();
+        final int amount = sc.nextInt();
         if (amount > 0) {
             balance += amount;
             System.out.println("thanks for depositing " + balance + " dollars at our bank");
-
         } else {
             System.out.println("that's not really possible");
         }
-
     }
 
-    void withdrawal(Scanner sc) {
+    public void withdrawal(final Scanner sc) {
+        // TODO same with here (see line 17)
         System.out.println("withdraw an amount of money: ");
         int amount = sc.nextInt();
         if (amount > 0) {
             balance -= amount;
             System.out.println("thanks for withdrawing " + balance + " dollars at our bank");
-
         }
     }
 
-    public void calculatedInterest(int years) {
-
+    public void calculatedInterest(final int years) {
+        // TODO Make this return a double
         double interestRate = .0238;
         double newBalance = balance * (1 + interestRate * years);
     }
 
     //todo use this somewhere please, it seems like a good concept but just not being used
-    void selectionMenu() throws IOException {
-        Scanner sc = new Scanner(System.in);
+    public void selectionMenu() {
+        // TODO call this in the main method
+        final Scanner sc = new Scanner(System.in);
         System.out.println("hey there! welcome to our bank! type an option to get started!");
         System.out.println("A: see your balance");
         System.out.println("B: make a deposit");
@@ -57,42 +53,85 @@ public class Account {
         System.out.println("E: calculate interest");
         System.out.println("F: quit the program");
 
+        char option;
         do {
             System.out.println("Enter an option: ");
-            this.option = sc.next();
+            option = Character.toUpperCase(sc.next().charAt(0));
             System.out.println();
 
-            if (option == null) return;
-
-            if (option.matches("[a-fA-F]")) {
-                switch ((option.toUpperCase())) {
-                    case "A":
-                        System.out.println("==================================================================================================================");
-                        System.out.println("your current balance is $ " + balance);
-                        System.out.println("==================================================================================================================");
-                    case "B":
-                        System.out.println("================================================================================================================== ");
-                        deposit(sc);
-                        System.out.println("==================================================================================================================");
-                    case "C":
-                        System.out.println("==================================================================================================================");
-                        withdrawal(sc);
-                        System.out.println("==================================================================================================================");
-                    case "D":
-                        System.out.println("Not yet implemented!");
-                    case "E":
-                        System.out.println("==================================================================================================================");
-                        calculatedInterest(5);
-                        System.out.println("==================================================================================================================");
-                    case "F":
-                        System.out.println("Thanks for using the bank!");
-                    default:
-                        System.out.println("Error! Invalid character detected.");
+            switch (option) {
+                case 'A' -> {
+                    System.out.println("==================================================================================================================");
+                    System.out.println("your current balance is $ " + balance);
+                    System.out.println("==================================================================================================================");
                 }
+                case 'B' -> {
+                    System.out.println("================================================================================================================== ");
+                    deposit(sc);
+                    System.out.println("==================================================================================================================");
+                }
+                case 'C' -> {
+                    System.out.println("==================================================================================================================");
+                    withdrawal(sc);
+                    System.out.println("==================================================================================================================");
+                }
+                case 'E' -> {
+                    System.out.println("==================================================================================================================");
+                    calculatedInterest(5);
+                    System.out.println("==================================================================================================================");
+                }
+                case 'F' -> System.out.println("Thanks for using the bank!");
+                default -> System.out.println("Error! Invalid character detected.");
             }
-        } while (option != null);
+        } while (option != 'F');
+    }
+
+    // TODO Remove some of these if you aren't using them.
+    public int getDepositTransaction() {
+        return depositTransaction;
+    }
+
+    public void setDepositTransaction(int depositTransaction) {
+        this.depositTransaction = depositTransaction;
+    }
+
+    public int getWithdrawTransaction() {
+        return withdrawTransaction;
+    }
+
+    public void setWithdrawTransaction(int withdrawTransaction) {
+        this.withdrawTransaction = withdrawTransaction;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public int getPreviousTransaction() {
+        return previousTransaction;
+    }
+
+    public void setPreviousTransaction(int previousTransaction) {
+        this.previousTransaction = previousTransaction;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(String customerID) {
+        this.customerID = customerID;
     }
 }
-
-
-
--- src/main/java/hydrate/AccountLoader.java
+++ src/main/java/hydrate/AccountLoader.java
@@ -3,15 +3,12 @@ package hydrate;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class Accountloader {
+public class AccountLoader {
     public static void main(String[] args) {
-
-
-        try ( FileWriter writer = new FileWriter("Accounts.txt")){
+        try (FileWriter writer = new FileWriter("Accounts.txt")) {
             //todo fill me !!!
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
 }
